762d331844f35d9463bd9c2bcde66dffe357c6ab
Jonata Hessa <jonata.hessa@gmail.com>
2018-10-15 20:09:14 -0300

    Validação sintática com Bean Validation

--- src/main/java/com/jonata/cursomc/services/CategoriaService.java
+++ src/main/java/com/jonata/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.jonata.cursomc.domain.Categoria;
+import com.jonata.cursomc.dto.CategoriaDTO;
 import com.jonata.cursomc.repositories.CategoriaRepository;
 import com.jonata.cursomc.services.exceptions.DataIntegrityException;
 import com.jonata.cursomc.services.exceptions.ObjectNotFoundException;
@@ -53,5 +54,9 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page,Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 }
